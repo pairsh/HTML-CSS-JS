022fe57e72533d8501633434286ef39cbc26e898
pairsh <489366184@qq.com>
2021-08-20 16:02:48 +0800

    测试

--- 01.js
+++ 01.js
@@ -1,6 +1,8 @@
-var a=10,b=12;
-if(a>=b){
-    document.write(a);
-}else{
-    document.write(b);
+function a(q,w){
+    try{
+        document.write(a)
+    }
+    catch(exception){
+        
+    }
 }
